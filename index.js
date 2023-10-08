0ae9229d34d1b925a1dc67b1c9a69f9f1a093bd2
AdamAshraREvaldy <adamashra2@gmail.com>
2023-10-09 00:14:43 +0700

    edit endpoint

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.post('/', function(req, res){
     res.send('test post!');
 })
 
-app.get('/', (req, res) =>{
+// Data Actor
+app.get('/actors', (req, res) =>{
     pool.query('SELECT * FROM actor', (err, result) =>{
         if(err){
             throw err;
@@ -21,57 +22,58 @@ app.get('/', (req, res) =>{
 })
 
 // Data Film
+
 // Get seluruh data film
-// app.get('/', (req, res) => {
-//     pool.query('SELECT * FROM film', (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-//       res.send(result.rows);
-//     });
-// });
+app.get('/films', (req, res) => {
+    pool.query('SELECT * FROM film', (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.rows);
+    });
+});
 
 // Menampilkan data film tertentu berdasarkan id
-// app.get('/films/:filmId', (req, res) => {
-//     const filmId = req.params.filmId;
-//     pool.query('SELECT * FROM film WHERE film_id = $1', [filmId], (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-//       res.send(result.rows);
-//     });
-//   });
+app.get('/films/:filmId', (req, res) => {
+    const filmId = req.params.filmId;
+    pool.query('SELECT * FROM film WHERE film_id = $1', [filmId], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.rows);
+    });
+  });
 
 // akhir data film
 
 // Data Category
-// app.get('/', (req, res) => {
-//     pool.query('SELECT * FROM category', (err, result) => {
-//         if (err) {
-//             throw err;
-//         }
-//         res.send(result.rows);
-//     });
-// });
+app.get('/category', (req, res) => {
+    pool.query('SELECT * FROM category', (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result.rows);
+    });
+});
 
 // Menampilkan data list film berdasarkan category
 
-// app.get('/films-by-category/:categoryId', (req, res) => {
-//     const categoryId = req.params.categoryId;
-//     pool.query(
-//         'SELECT film.film_id, film.title, film.description, film_category.category_id ' +
-//         'FROM film ' +
-//         'INNER JOIN film_category ON film.film_id = film_category.film_id ' +
-//         'WHERE film_category.category_id = $1',
-//         [categoryId],
-//         (err, result) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             res.send(result.rows);
-//         }
-//     );
-// });
+app.get('/films-by-category/:categoryId', (req, res) => {
+    const categoryId = req.params.categoryId;
+    pool.query(
+        'SELECT film.film_id, film.title, film.description, film_category.category_id ' +
+        'FROM film ' +
+        'INNER JOIN film_category ON film.film_id = film_category.film_id ' +
+        'WHERE film_category.category_id = $1',
+        [categoryId],
+        (err, result) => {
+            if (err) {
+                throw err;
+            }
+            res.send(result.rows);
+        }
+    );
+});
 
 
 
